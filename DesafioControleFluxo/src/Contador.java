4bfe83b28a93feab005b2dcdc82b36e8d179afa2
Patrick Lopes <lopes.patrick4@gmail.com>
2024-04-26 13:31:09 -0300

    Add initials

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do primeiro parametro: ");
+        int primeiroParametro = sc.nextInt();
+
+        System.out.print("Digite o valor do segundo parametro: ");
+        int segundoParametro = sc.nextInt();
+    }
+}
